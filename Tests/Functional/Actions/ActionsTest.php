cb7878b8b071b075454a298c004e891a6a9232fa
Marc Neuhaus <apocalip@gmail.com>
2012-06-16 01:19:21 +0200

    [TASK] Added the Functional Test for the ListController

---
+++ Tests/Functional/Actions/ActionsTest.php
@@ -0,0 +1,94 @@
+<?php
+namespace Foo\ContentManagement\Tests\Functional\Actions;
+
+/*                                                                        *
+ * This script belongs to the Foo.ContentManagement package.              *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU Lesser General Public License, either version 3   *
+ *  of the License, or (at your option) any later version.                *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+use TYPO3\FLOW3\Http\Request;
+use TYPO3\FLOW3\Http\Uri;
+use TYPO3\FLOW3\Mvc\ActionRequest;
+use Symfony\Component\DomCrawler\Crawler;
+
+/**
+ * Testcase for Standalone View
+ *
+ * @group large
+ */
+class ActionsTest extends \TYPO3\FLOW3\Tests\FunctionalTestCase {
+
+	/**
+	 * @var boolean
+	 */
+	protected $testableHttpEnabled = TRUE;
+
+	/**
+	 * @var boolean
+	 */
+	static protected $testablePersistenceEnabled = TRUE;
+
+	/**
+	 * @var \TYPO3\FLOW3\Http\Client\Browser
+	 */
+	protected $browser;
+
+	/**
+	 * Initializer
+	 */
+	public function setUp() {
+		parent::setUp();
+
+		$route = new \TYPO3\FLOW3\Mvc\Routing\Route();
+		$route->setUriPattern('test/contentmanagement/actions(/{@action})');
+		$route->setDefaults(array(
+			'@package' => 'Foo.ContentManagement',
+			'@subpackage' => 'Tests\Functional\Actions\Fixtures',
+			'@controller' => 'Actions',
+			'@action' => 'index',
+			'@format' =>'html'
+		));
+		$route->setAppendExceedingArguments(TRUE);
+		$this->router->addRoute($route);
+	}
+
+	public function createDummyPost() {
+		$post = new Fixtures\Domain\Model\Post();
+		$post->setEmail('foo@bar.org');
+		$post->setName('myName');
+		$this->persistenceManager->add($post);
+		$postIdentifier = $this->persistenceManager->getIdentifierByObject($post);
+		$this->persistenceManager->persistAll();
+		$this->persistenceManager->clearState();
+
+		return $postIdentifier;
+	}
+	
+	/**
+	 * @test
+	 */
+	public function postListIsRendered() {
+		$this->createDummyPost();
+
+		$uriArguments = array(
+			'--actionRuntime' => array(
+				'being' => 'Foo\ContentManagement\Tests\Functional\Actions\Fixtures\Domain\Model\Post',
+				'@action' => 'index',
+				'@controller' => 'list',
+				'@package' => 'foo.contentmanagement'
+			)
+		);
+		$class = "Foo\ContentManagement\Tests\Functional\Actions\Fixtures\Domain\Model\Post";
+		$this->browser->request('http://localhost/test/contentmanagement/actions?' . http_build_query($uriArguments));
+		
+		$content = $this->browser->getLastResponse()->getContent();
+
+		$this->assertTrue((boolean) stristr($content, "foo@bar.org"));
+	}
+}
+?>
---
+++ Tests/Functional/Actions/Fixtures/Controller/ActionsController.php
@@ -0,0 +1,61 @@
+<?php
+namespace Foo\ContentManagement\Tests\Functional\Actions\Fixtures\Controller;
+
+/*                                                                        *
+ * This script belongs to the Foo.ContentManagement package.              *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU Lesser General Public License, either version 3   *
+ *  of the License, or (at your option) any later version.                *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+/**
+ * Controller for simple CRUD actions, to test Fluid forms in
+ * combination with Property Mapping
+ */
+class ActionsController extends \TYPO3\FLOW3\Mvc\Controller\ActionController {
+
+	public function setTemplate($action) {
+		$this->view->setTemplatePathAndFilename("Packages/Application/Foo.ContentManagement/Tests/Functional/Actions/Fixtures/Templates/" . $action . ".html");
+	}
+
+	/**
+	 * Prepares a view for the current action and stores it in $this->view.
+	 * By default, this method tries to locate a view with a name matching
+	 * the current action.
+	 *
+	 * @return \TYPO3\FLOW3\Mvc\View\ViewInterface the resolved view
+	 * @api
+	 */
+	protected function resolveView() {
+		$view = new \TYPO3\Fluid\View\StandaloneView();
+		$view->setControllerContext($this->controllerContext);
+		return $view;
+	}
+
+
+	/**
+	 * Display a start page
+	 *
+	 * @return void
+	 */
+	public function indexAction() {
+		$this->setTemplate("Index");
+	}
+
+	// /**
+	//  * @param \TYPO3\Fluid\Tests\Functional\Form\Fixtures\Domain\Model\Post $post
+	//  * @return void
+	//  */
+	// public function createAction(\TYPO3\Fluid\Tests\Functional\Form\Fixtures\Domain\Model\Post $post) {
+	// 	return $post->getName() . '|' . $post->getEmail();
+	// }
+	// 
+	// 
+	public function resolveViewObjectName() {
+		return $this->defaultViewObjectName;
+	}
+}
+?>
