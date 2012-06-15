b7a47d3686cf9261bafea28e912b5b3b42775135
Marc Neuhaus <apocalip@gmail.com>
2012-06-16 01:10:14 +0200

    [BUGFIX] Fix a but with obsolete redirect handling

--- Classes/Controller/ListController.php
+++ Classes/Controller/ListController.php
@@ -50,7 +50,7 @@ class ListController extends \Foo\ContentManagement\Core\Actions\AbstractAction 
 			// Redirect to creating a new Object if there aren't any (Clean Slate)
 			if( $results->count() < 1 ) {
 				$arguments = array("being" => $this->persistentStorageService->getClassShortName($this->being));
-				$this->actionManager->redirect("create", $arguments);
+				$this->redirect("create", NULL, NULL, $arguments);
 			}
 			
 			$listActions = $this->actionManager->getActions("list", $this->being, true);
