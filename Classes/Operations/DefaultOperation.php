b84b17272c987d1d76539076444591e8a4d8e548
Marc Neuhaus <apocalip@gmail.com>
2012-08-01 08:43:42 +0200

    [FEAUTURE] Added some operations
    - DefaultOperation
    - EmptyOperation
    - NotEmptyOperation

---
+++ Classes/Operations/DefaultOperation.php
@@ -0,0 +1,53 @@
+<?php
+namespace Foo\ContentManagement\Operations;
+
+/*                                                                        *
+ * This script belongs to the FLOW3 package "TYPO3.Eel".                  *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU Lesser General Public License, either version 3   *
+ *  of the License, or (at your option) any later version.                *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+use TYPO3\FLOW3\Annotations as FLOW3;
+
+/**
+ * Count the number of elements in the context, possibly filtering them before counting.
+ */
+class DefaultOperation extends \TYPO3\Eel\FlowQuery\Operations\AbstractOperation  {
+
+	/**
+	 * {@inheritdoc}
+	 *
+	 * @var string
+	 */
+	static protected $shortName = 'default';
+
+	/**
+	 * {@inheritdoc}
+	 *
+	 * @var boolean
+	 */
+	static protected $final = TRUE;
+
+	/**
+	 * {@inheritdoc}
+	 *$
+	 * @param \TYPO3\Eel\FlowQuery\FlowQuery $flowQuery the FlowQuery object
+	 * @param array $arguments the arguments for this operation
+	 * @return mixed|null if the operation is final, the return value
+	 */
+	public function evaluate(\TYPO3\Eel\FlowQuery\FlowQuery $flowQuery, array $arguments) {
+		if (count($arguments) == 1) {
+			$value = $flowQuery->getContext();
+			return empty($value[0]) ? $arguments[0] : $value[0];
+		} else {
+			$flowQuery->pushOperation('default', array());
+			$flowQuery->pushOperation('filter', $arguments);
+		}
+	}
+}
+
+?>
