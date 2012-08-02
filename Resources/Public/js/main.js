56c69b5412ef5f04b45f0c3a78aa34d213bc2c24
Marc Neuhaus <apocalip@gmail.com>
2012-08-02 23:02:44 +0200

    [BUGFIX] Fixed InlineHelper Behavior and styling

--- Resources/Public/js/main.js
+++ Resources/Public/js/main.js
@@ -1,10 +1,10 @@
 jQuery(document).ready(function(){
 	jQuery("input[type='text']:first").focus();
 
-	jQuery(document).ready(function(){
-      jQuery("[data-klove=container]").klove("table");
-      jQuery("[data-klove-shortcut]").klove();
-    });
+	// jQuery(document).ready(function(){
+ //      jQuery("[data-klove=container]").klove("table");
+ //      jQuery("[data-klove-shortcut]").klove();
+ //    });
 
 	// Table select all behavior
 	jQuery(".select-all").change(function(){
@@ -24,40 +24,40 @@ jQuery(document).ready(function(){
 	});
 		
 	//jQuery("#shortcuts-modal").modal({show: false, background: true, keyboard: true});
-	jQuery("#shortcuts-modal .modal-body").append("<dl class='hotkeys'><dt>h</dt><dd>toggle this help dialog</dd></dl>");
-	jwerty.key("h", function () {
-		var tag = document.activeElement.tagName;
-		if(tag == "INPUT" || tag == "SELECT" || tag == "TEXTAREA") return;
-		jQuery("#shortcuts-modal").toggle();
-	});
-	jQuery("#shortcuts-modal .close").click(function(){
-		jQuery("#shortcuts-modal").hide();
-	});
+	// jQuery("#shortcuts-modal .modal-body").append("<dl class='hotkeys'><dt>h</dt><dd>toggle this help dialog</dd></dl>");
+	// jwerty.key("h", function () {
+	// 	var tag = document.activeElement.tagName;
+	// 	if(tag == "INPUT" || tag == "SELECT" || tag == "TEXTAREA") return;
+	// 	jQuery("#shortcuts-modal").toggle();
+	// });
+	// jQuery("#shortcuts-modal .close").click(function(){
+	// 	jQuery("#shortcuts-modal").hide();
+	// });
 	
-	jQuery("[data-klove-shortcut]").each(function(){
-		var e = jQuery(this);
+	// jQuery("[data-klove-shortcut]").each(function(){
+	// 	var e = jQuery(this);
 		
-		if(e.attr("data-klove-info") !== undefined)
-			var text = e.attr("data-klove-info");
-		else if(e.attr("type") == "checkbox")
-			var text = "toggle checkbox";
-		else
-			var text = e.text();
+	// 	if(e.attr("data-klove-info") !== undefined)
+	// 		var text = e.attr("data-klove-info");
+	// 	else if(e.attr("type") == "checkbox")
+	// 		var text = "toggle checkbox";
+	// 	else
+	// 		var text = e.text();
 			
-		jQuery("#shortcuts-modal .modal-body").append("<dl class='hotkeys'><dt>" + e.attr("data-klove-shortcut") + "</dt><dd>" + text + "</dd></dl>");
-	});
-	jQuery("#shortcuts-modal .modal-body").append('<h4 class="break">Shortcuts for current item</h4>');
-	jQuery("[data-klove=row]:first [data-klove-row-shortcut]").each(function(){
-		var e = jQuery(this);
+	// 	jQuery("#shortcuts-modal .modal-body").append("<dl class='hotkeys'><dt>" + e.attr("data-klove-shortcut") + "</dt><dd>" + text + "</dd></dl>");
+	// });
+	// jQuery("#shortcuts-modal .modal-body").append('<h4 class="break">Shortcuts for current item</h4>');
+	// jQuery("[data-klove=row]:first [data-klove-row-shortcut]").each(function(){
+	// 	var e = jQuery(this);
 		
-		var key = e.attr("data-klove-row-shortcut");
-		if(e.attr("data-klove-info") !== undefined)
-			var text = e.attr("data-klove-info");
-		else if(e.attr("type") == "checkbox")
-			var text = "toggle checkbox";
-		else
-			var text = e.text();
+	// 	var key = e.attr("data-klove-row-shortcut");
+	// 	if(e.attr("data-klove-info") !== undefined)
+	// 		var text = e.attr("data-klove-info");
+	// 	else if(e.attr("type") == "checkbox")
+	// 		var text = "toggle checkbox";
+	// 	else
+	// 		var text = e.text();
 		
-		jQuery("#shortcuts-modal .modal-body").append("<dl class='hotkeys'><dt>" + key + "</dt><dd>" + text + "</dd></dl>");
-	});
+	// 	jQuery("#shortcuts-modal .modal-body").append("<dl class='hotkeys'><dt>" + key + "</dt><dd>" + text + "</dd></dl>");
+	// });
 });
