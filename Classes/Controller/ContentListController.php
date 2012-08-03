ac4c3ac1d747690a934d66ad80628a0b392857de
Sebastian Kurfuerst <sebastian@typo3.org>
2012-08-03 15:06:07 +0200

    WIP: Node listing

---
+++ Classes/Controller/ContentListController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Foo\ContentManagement\Controller;
+
+/* *
+ * This script belongs to the Foo.ContentManagement package.              *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU Lesser General Public License as published by the *
+ * Free Software Foundation, either version 3 of the License, or (at your *
+ * option) any later version.                                             *
+ *                                                                        *
+ * This script is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHAN-    *
+ * TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser       *
+ * General Public License for more details.                               *
+ *                                                                        *
+ * You should have received a copy of the GNU Lesser General Public       *
+ * License along with the script.                                         *
+ * If not, see http://www.gnu.org/licenses/lgpl.html                      *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+use Doctrine\ORM\Mapping as ORM;
+use TYPO3\FLOW3\Annotations as FLOW3;
+use TYPO3\FLOW3\Mvc\ActionRequest;
+
+/**
+ * Feature to display a list of TYPO3CR content nodes
+ *
+ * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License, version 3 or later
+ */
+class ContentListController extends \Foo\ContentManagement\Core\Features\AbstractFeature {
+	protected $defaultViewObjectName = 'TYPO3\TypoScript\View\TypoScriptView';
+
+	/**
+	 * @FLOW3\Inject
+	 * @var \TYPO3\FLOW3\Property\PropertyMapper
+	 */
+	protected $propertyMapper;
+
+	/**
+	 * List objects, all being of the same $type.
+	 *
+	 * TODO: Filtering of this list, bulk
+	 *
+	 * @param string $format
+	 * @param TYPO3\TYPO3CR\Domain\Model\NodeInterface $selectedFolderNode
+	 */
+	public function indexAction($format = 'table', \TYPO3\TYPO3CR\Domain\Model\NodeInterface $selectedFolderNode = NULL) {
+		$siteNode = $this->getSiteNode();
+
+		$this->view->assign('format', $format);
+		$this->view->assign('siteNode', $siteNode);
+		$this->view->assign('selectedFolderNode', $selectedFolderNode);
+
+		if ($selectedFolderNode !== NULL) {
+			$contentNodes = $this->getContentElements($selectedFolderNode, TRUE);
+			$this->view->assign('objects', $contentNodes);
+		}
+	}
+
+	/**
+	 * @return \TYPO3\TYPO3CR\Domain\Model\NodeInterface
+	 */
+	protected function getSiteNode() {
+		return $this->propertyMapper->convert('/sites', 'TYPO3\TYPO3CR\Domain\Model\NodeInterface');
+	}
+
+	/**
+	 *
+	 * !!! RECURSIVE FUNCTION
+	 *
+	 * @param \TYPO3\TYPO3CR\Domain\Model\NodeInterface $node
+	 * @param boolean $recursive
+	 * @return array
+	 */
+	protected function getContentElements(\TYPO3\TYPO3CR\Domain\Model\NodeInterface $node = NULL, $recursive) {
+		if ($node === NULL) {
+			return array();
+		}
+
+		$contentTypeFilter = NULL;
+		if ($recursive === FALSE) {
+			$contentTypeFilter = '!TYPO3.TYPO3:Page';
+		}
+		$childNodes = $node->getChildNodes($contentTypeFilter);
+		$result = $childNodes;
+		foreach ($childNodes as $childNode) {
+			$result = array_merge($result, $this->getContentElements($childNode, $recursive));
+		}
+
+
+		return $result;
+	}
+}
+?>
---
+++ Classes/EelNodeLabelGenerator.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Foo\ContentManagement;
+
+/* *
+ * This script belongs to the Foo.ContentManagement package.              *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU Lesser General Public License as published by the *
+ * Free Software Foundation, either version 3 of the License, or (at your *
+ * option) any later version.                                             *
+ *                                                                        *
+ * This script is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHAN-    *
+ * TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser       *
+ * General Public License for more details.                               *
+ *                                                                        *
+ * You should have received a copy of the GNU Lesser General Public       *
+ * License along with the script.                                         *
+ * If not, see http://www.gnu.org/licenses/lgpl.html                      *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+use TYPO3\FLOW3\Annotations as FLOW3;
+
+/**
+ * Eel Node Label Generator. Should be moved to permanent location lateron.
+ *
+ * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License, version 3 or later
+ * @FLOW3\Scope("singleton")
+ */
+
+/**
+ * A default Node label generator
+ */
+class EelNodeLabelGenerator implements \TYPO3\TYPO3CR\Domain\Model\NodeLabelGeneratorInterface {
+
+	/**
+	 * @FLOW3\Inject
+	 * @var \TYPO3\Eel\EelEvaluatorInterface
+	 */
+	protected $eelEvaluator;
+
+	/**
+	 * @FLOW3\Inject
+	 * @var \TYPO3\TYPO3CR\Domain\Service\ContentTypeManager
+	 */
+	protected $contentTypeManager;
+
+	/**
+	 * Generate a default label for a node from an Eel expression
+	 *
+	 * @param \TYPO3\TYPO3CR\Domain\Model\NodeInterface $node
+	 * @return string
+	 */
+	public function getLabel(\TYPO3\TYPO3CR\Domain\Model\NodeInterface $node) {
+		$contentType = $this->contentTypeManager->getContentType($node->getContentType());
+		$options = $contentType->getNodeLabelGeneratorOptions();
+
+		$variables = array(
+			'context' => new \TYPO3\Eel\FlowQuery\FlowQuery(array($node)),
+			'strings' => new \TYPO3\Eel\Helper\StringHelper()
+		);
+		return $this->eelEvaluator->evaluate($options['expression'], new \TYPO3\Eel\Context($variables));
+	}
+}
+?>
