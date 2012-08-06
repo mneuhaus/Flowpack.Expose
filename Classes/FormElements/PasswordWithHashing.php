602a6182293606d142267d7eeddd130fe050aef7
Marc Neuhaus <apocalip@gmail.com>
2012-08-06 11:11:04 +0200

    [TASK] Added User administration

---
+++ Classes/FormElements/PasswordWithHashing.php
@@ -0,0 +1,40 @@
+<?php
+namespace Foo\ContentManagement\FormElements;
+
+/*                                                                        *
+ * This script belongs to the FLOW3 package "TYPO3.Form".                 *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU Lesser General Public License, either version 3   *
+ *  of the License, or (at your option) any later version.                *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+use TYPO3\FLOW3\Annotations as FLOW3;
+
+/**
+ * A password with confirmation form element
+ */
+class PasswordWithHashing extends \TYPO3\Form\FormElements\PasswordWithConfirmation {
+	/**
+	 * @var \TYPO3\FLOW3\Security\Cryptography\HashService
+	 * @FLOW3\Inject
+	 */
+	protected $hashService;
+
+	public function onSubmit(\TYPO3\Form\Core\Runtime\FormRuntime $formRuntime, &$elementValue) {
+		if ($elementValue['password'] !== $elementValue['confirmation']) {
+			$processingRule = $this->getRootForm()->getProcessingRule($this->getIdentifier());
+			$processingRule->getProcessingMessages()->addError(new \TYPO3\FLOW3\Error\Error('Password doesn\'t match confirmation', 1334768052));
+		}
+
+		if (empty($elementValue['password'])){
+			$elementValue = $this->getDefaultValue();
+		} else {
+			$elementValue = $this->hashService->hashPassword($elementValue['password'], "default");
+		}
+	}
+
+}
+?>
---
+++ Classes/OptionsProvider/RoleOptionsProvider.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Foo\ContentManagement\OptionsProvider;
+
+/*                                                                        *
+* This script belongs to the Foo.ContentManagement package.              *
+*                                                                        *
+* It is free software; you can redistribute it and/or modify it under    *
+* the terms of the GNU Lesser General Public License as published by the *
+* Free Software Foundation, either version 3 of the License, or (at your *
+* option) any later version.                                             *
+*                                                                        *
+* This script is distributed in the hope that it will be useful, but     *
+* WITHOUT ANY WARRANTY; without even the implied warranty of MERCHAN-    *
+* TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser       *
+* General Public License for more details.                               *
+*                                                                        *
+* You should have received a copy of the GNU Lesser General Public       *
+* License along with the script.                                         *
+* If not, see http://www.gnu.org/licenses/lgpl.html                      *
+*                                                                        *
+* The TYPO3 project - inspiring people to share!                         *
+*                                                                        */
+
+use Doctrine\ORM\Mapping as ORM;
+use TYPO3\FLOW3\Annotations as FLOW3;
+
+/**
+ * OptionsProvider for related Beings
+ *
+ * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License, version 3 or later
+ */
+class RoleOptionsProvider extends \Foo\ContentManagement\Core\OptionsProvider\AbstractOptionsProvider {
+	/**
+	 * @var \TYPO3\FLOW3\Configuration\ConfigurationManager
+	 * @FLOW3\Inject
+	 */
+	protected $configurationManager;
+
+	public function getOptions(){
+		$acls = $this->configurationManager->getConfiguration(\TYPO3\FLOW3\Configuration\ConfigurationManager::CONFIGURATION_TYPE_POLICY, "acls");
+
+		return array_keys($acls);
+	}
+}
+
+?>
