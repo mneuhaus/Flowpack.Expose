f5fbe2f2a3951523b9fc91b0a75ab7da743208a6
Sebastian Kurfuerst <sebastian@typo3.org>
2012-09-04 19:56:54 +0200

    [FEATURE] node preview

---
+++ Classes/TypoScriptObjects/IgnoreMissingTypeMatcher.php
@@ -0,0 +1,32 @@
+<?php
+namespace TYPO3\Admin\TypoScriptObjects;
+
+/*                                                                        *
+ * This script belongs to the TYPO3.Admin package.              		  *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU General Public License, either version 3 of the   *
+ * License, or (at your option) any later version.                        *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+use TYPO3\FLOW3\Annotations as FLOW3;
+
+/**
+ * A matcher which only matches if the target typoscript type is actually found
+ */
+class IgnoreMissingTypeMatcher extends \TYPO3\TypoScript\TypoScriptObjects\Matcher {
+
+	public function evaluate() {
+		try {
+			return parent::evaluate();
+		} catch(\TYPO3\TypoScript\Exception $e) {
+			if ($e->getCode() === 1332493995) {
+					// BAD HACK!!
+				return \TYPO3\TypoScript\TypoScriptObjects\CaseTsObject::MATCH_NORESULT;
+			}
+		}
+	}
+}
+?>
