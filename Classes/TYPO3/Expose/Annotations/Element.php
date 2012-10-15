7dd9168f604eeee50e7149cb49849086225f22f2
Marc Neuhaus <apocalip@gmail.com>
2012-10-15 17:01:24 +0200

    [TASK] Reimplemented Element Annotation
    Change-Id: I5a62fc8fded5f85452afdd142ef7933ce830d69c

---
+++ Classes/TYPO3/Expose/Annotations/Element.php
@@ -0,0 +1,42 @@
+<?php
+namespace TYPO3\Expose\Annotations;
+
+/*                                                                        *
+ * This script belongs to the TYPO3.Expose package.              *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU Lesser General Public License, either version 3   *
+ * of the License, or (at your option) any later version.                 *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+/**
+ * @Annotation
+ */
+final class Element {
+
+    /**
+     * @var string
+     */
+    public $name = '';
+
+    /**
+     * @param string $value
+     */
+    public function __construct(array $values) {
+        if (isset($values['value'])) {
+            $this->name = $values['value'];
+        }
+    }
+
+    /**
+    * TODO: Document this Method! ( __toString )
+    */
+    public function __toString() {
+        return $this->name;
+    }
+
+}
+
+?>
