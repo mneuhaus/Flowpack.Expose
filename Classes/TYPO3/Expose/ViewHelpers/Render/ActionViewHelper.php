edbb3e1197d3c4e6907d05af24af0150e8f746db
Marc Neuhaus <mneuhaus@famelo.com>
2012-11-20 15:40:02 +0100

    [FEATURE] renamed actionViewHelper arguments
    Change-Id: I45ad809db610d86cee45a6ed3c095a412ab3af80

--- Classes/TYPO3/Expose/ViewHelpers/Render/ActionViewHelper.php
+++ Classes/TYPO3/Expose/ViewHelpers/Render/ActionViewHelper.php
@@ -23,29 +23,35 @@ use TYPO3\Flow\Annotations as Flow;
 class ActionViewHelper extends \TYPO3\Fluid\Core\ViewHelper\AbstractViewHelper {
 
 	/**
-	 * @param string $defaultAction name of the entry action
-	 * @param string $defaultController name of the entry controller
-	 * @param string $defaultPacakge name of the entry package
-	 * @param array $defaultArguments for the controller
+	 * @param string $action name of the entry action
+	 * @param string $controller name of the entry controller
+	 * @param string $package name of the entry package
+	 * @param array $arguments for the controller
 	 * @param string $type to use for the action
+	 * @param object $object to use for the action
 	 * @param string $typoScriptPrefix prototype prefix for the TypoScript rendering
 	 * @return string the rendered form
 	 */
-	public function render($defaultAction = 'index', $defaultController = 'Index', $defaultPackage = 'TYPO3.Expose', $defaultArguments = array(), $type = NULL, $typoScriptPrefix = NULL) {
+	public function render($action = 'index', $controller = 'Index', $package = 'TYPO3.Expose', $arguments = array(), $type = NULL, $object = NULL, $typoScriptPrefix = NULL) {
 		$response = new \TYPO3\Flow\Http\Response($this->controllerContext->getResponse());
 		$request = $this->controllerContext->getRequest();
 
 		if ($type !== NULL) {
-			$defaultArguments['type'] = $type;
+			$arguments['type'] = $type;
+		}
+
+		if ($object !== NULL) {
+			$arguments['type'] = get_class($object);
+			$arguments['objects']['0'] = $object;
 		}
 
 		$exposeRuntime = new \TYPO3\Expose\Core\ExposeRuntime($request, $response);
-        $exposeRuntime->setDefaultPackage($defaultPackage);
-        $exposeRuntime->setDefaultController($defaultController);
-        $exposeRuntime->setDefaultAction($defaultAction);
-        $exposeRuntime->setDefaultArguments($defaultArguments);
-        $exposeRuntime->setTypoScriptPrefix($typoScriptPrefix);
-        return $exposeRuntime->execute();
+		$exposeRuntime->setDefaultPackage($package);
+		$exposeRuntime->setDefaultController($controller);
+		$exposeRuntime->setDefaultAction($action);
+		$exposeRuntime->setDefaultArguments($arguments);
+		$exposeRuntime->setTypoScriptPrefix($typoScriptPrefix);
+		return $exposeRuntime->execute();
 	}
 }
 
