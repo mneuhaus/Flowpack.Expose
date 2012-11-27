ab02cc60f07dd1fbb8277e83b30e907e999113be
Marc Neuhaus <mneuhaus@famelo.com>
2012-11-27 01:27:14 +0100

    [BUGFIX] Readd inlineHelper script
    Change-Id: Ifbe8e17a4a0976c49bb24f6dc3b4e70fd83511a7

--- Resources/Public/JavaScript/Form/Form.js
+++ Resources/Public/JavaScript/Form/Form.js
@@ -3,5 +3,6 @@ jQuery(document).ready(function(){
 	jQuery(".t3-expose-inline[data-mode='multiple']").parents("form").submit(function(){
 		var form = jQuery(this);
 		form.find(".t3-expose-inline-item-unused").remove();
+		// TODO: Remove the name attr from all form elements beneath unused instead
 	});
 });
