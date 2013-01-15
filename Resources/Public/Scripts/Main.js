445284d39b7cee4d17ebe22bd7b03c885af6bbe3
Marc Neuhaus <mneuhaus@famelo.com>
2013-01-15 23:37:41 +0100

    [CLEANUP] Changed Resource Folders to suggested names
    Based on the suggestions in https://review.typo3.org/#/c/17126/
    
    Change-Id: I6b1a9ea2476f72bfa1103b193cbab96ce55ea41c

---
+++ Resources/Public/Scripts/Main.js
@@ -0,0 +1,22 @@
+// @codekit-prepend "Form/jquery.inlinehelper.js"
+// @codekit-prepend "Form/Form.js"
+// @codekit-prepend "../components/chosen/chosen/chosen.jquery.min.js"
+
+jQuery(document).ready(function(){
+	jQuery("input[type='text']:first").focus();
+
+	// Table select all behavior
+	jQuery(".select-all").change(function(){
+		var table = jQuery(this).parents("table");
+		if(jQuery(this).attr('checked') == "checked")
+			table.find(".select-row").attr('checked', "checked");
+		else
+			table.find(".select-row").removeAttr('checked');
+	});
+
+	jQuery(".inline[data-mode='multiple']").inlineHelper();
+	jQuery(".inline[data-mode='multiple']").parents("form").submit(function(){
+		var form = jQuery(this);
+		form.find(".inline-unused").remove();
+	});
+});
