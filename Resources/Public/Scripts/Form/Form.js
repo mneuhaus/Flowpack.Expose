c9a04ff00c50437ab7b4e9f1833b8ce7b40900b6
Marc Neuhaus <mneuhaus@famelo.com>
2014-07-08 23:35:14 +0200

    [TASK] Spring cleaning

--- Resources/Public/Scripts/Form/Form.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function(){
-	$(".t3-expose-inline[data-mode='multiple']").inlineHelper();
-	$(".chosen").chosen();
-});
--- Resources/Public/Scripts/recordlist-module.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-	$('body').addClass('js');
-
-	var $recordListSelector = $('.typo3-expose-recordlist');
-	if ($recordListSelector.length > 0) {
-		T3.Expose.RecordList.init($recordListSelector);
-	}
-});
